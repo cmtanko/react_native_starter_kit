fbc88c66d8ffa355d5c5bd47b76942dbc47fef8d
Suchan Badyakar <suchan211@gmail.com>
2020-11-28 20:02:12 +1100

    Add NativeBase for UI

---
+++ src/theme/categoryColors.js
@@ -0,0 +1,12 @@
+import theme from './variables/expenseManager';
+
+export default (categoryColors = [
+  theme.brandSuccess,
+  theme.brandSecondary,
+  theme.brandThird,
+  theme.brandWarning,
+  theme.brandFourth,
+  theme.brandInfo,
+  theme.brandPrimary,
+  theme.brandDanger,
+]);
