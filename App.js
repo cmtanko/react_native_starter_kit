b228d5241eb8111d6bf4ebd465be6db1615befce
Suchan Badyakar <suchan211@gmail.com>
2020-12-02 08:47:22 +1100

    Add eslint pretify husky

--- App.js
+++ App.js
@@ -1,31 +1,25 @@
 import * as React from 'react';
 import {Provider} from 'react-redux';
-import {Root, StyleProvider} from 'native-base';
+import {StyleProvider} from 'native-base';
 import configureStore from './src/store/configureStore';
 
-import getTheme from './native-base-theme/components';
-import { Container, Header, Content, Spinner } from 'native-base';
+import getTheme from "./native-base-theme/components";
+import {Container, Header, Content, Spinner} from 'native-base';
 const store = configureStore();
-
-if (__DEV__) {
-  console.disableYellowBox = true;
-  GLOBAL.XMLHttpRequest =
-    GLOBAL.originalXMLHttpRequest || GLOBAL.XMLHttpRequest;
-}
 
 export default function App() {
   return (
     <StyleProvider style={getTheme()}>
       <Provider store={store}>
-      <Container>
-        <Header />
-        <Content>
-          <Spinner />
-          <Spinner color='red' />
-          <Spinner color='green' />
-          <Spinner color='blue' />
-        </Content>
-      </Container>
+        <Container>
+          <Header />
+           <Content>
+               <Spinner />
+            <Spinner color="red" />
+            <Spinner color="green" />
+            <Spinner color="blue" />
+          </Content>
+        </Container>
       </Provider>
     </StyleProvider>
   );
