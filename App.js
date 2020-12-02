a580e067ba29f18ac993ae13057fec08d2c36559
Suchan Badyakar <suchan211@gmail.com>
2020-12-02 19:48:09 +1100

    add linter prettifier

--- App.js
+++ App.js
@@ -1,26 +1,3 @@
-import * as React from 'react';
-import {Provider} from 'react-redux';
-import {StyleProvider} from 'native-base';
-import configureStore from './src/store/configureStore';
+import App from './src/index';
 
-import getTheme from './native-base-theme/components';
-import {Container, Header, Content, Spinner} from 'native-base';
-const store = configureStore();
-
-export default function App() {
-  return (
-    <StyleProvider style={getTheme()}>
-      <Provider store={store}>
-        <Container>
-          <Header />
-          <Content>
-            <Spinner />
-            <Spinner color="red" />
-            <Spinner color="green" />
-            <Spinner color="blue" />
-          </Content>
-        </Container>
-      </Provider>
-    </StyleProvider>
-  );
-}
+export default App;
---
+++ src/index.js
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import {Provider} from 'react-redux';
+import {StyleProvider} from 'native-base';
+import configureStore from './store/configureStore';
+
+import getTheme from '../native-base-theme/components';
+import {Container, Header, Content, Spinner} from 'native-base';
+const store = configureStore();
+
+export default function App() {
+  return (
+    <StyleProvider style={getTheme()}>
+      <Provider store={store}>
+        <Container>
+          <Header />
+          <Content>
+            <Spinner />
+            <Spinner color="red" />
+            <Spinner color="green" />
+            <Spinner color="blue" />
+          </Content>
+        </Container>
+      </Provider>
+    </StyleProvider>
+  );
+}
