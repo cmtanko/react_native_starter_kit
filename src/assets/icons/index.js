066b96f2cbf326a69351aed23363393fb636f8b2
Suchan Badyakar <suchan211@gmail.com>
2021-04-12 09:44:41 +1000

    [APP-006-add-svg-icons] icons added

---
+++ src/assets/icons/index.js
@@ -0,0 +1,77 @@
+import bill from './bill';
+import car from './car';
+import card from './card';
+import carpay from './carpay';
+import dinning from './dinning';
+import donation from './donation';
+import earning from './earning';
+import education from './education';
+import electricity from './electricity';
+import electronics from './electronics';
+import entertainment from './entertainment';
+import family from './family';
+import fitness from './fitness';
+import food from './food';
+import fuel from './fuel';
+import garden from './garden';
+import gas from './gas';
+import gift from './gift';
+import grocery from './grocery';
+import home from './home';
+import insurance from './insurance';
+import loan from './loan';
+import medical from './medical';
+import mobile from './mobile';
+import office from './office';
+import others from './others';
+import personal from './personal';
+import rent from './rent';
+import rentpay from './rentpay';
+import salary from './salary';
+import settings from './settings';
+import shop from './shop';
+import transfer from './transfer';
+import transport from './transport';
+import travel from './travel';
+import water from './water';
+import wedding from './wedding';
+
+export const allIcons = {
+  bill,
+  car,
+  card,
+  carpay,
+  dinning,
+  donation,
+  earning,
+  education,
+  electricity,
+  electronics,
+  entertainment,
+  family,
+  fitness,
+  food,
+  fuel,
+  garden,
+  gas,
+  gift,
+  grocery,
+  home,
+  insurance,
+  loan,
+  medical,
+  mobile,
+  office,
+  others,
+  personal,
+  rent,
+  rentpay,
+  salary,
+  settings,
+  shop,
+  transfer,
+  transport,
+  travel,
+  water,
+  wedding,
+};
