907df89b38c46422635283bc853e9c11de85e73a
Suchan Badyakar <suchan211@gmail.com>
2021-04-24 17:53:38 +1000

    update

--- src/components/Common/index.js
+++ src/components/Common/index.js
@@ -1,8 +1,17 @@
 import IconBox from './IconBox';
+import ImageBox from './ImageBox';
 import InputBox from './InputBox';
 import ErrorBox from './ErrorBox';
 import ButtonBox from './ButtonBox';
 import PickerBox from './PickerBox';
 import DatePickerBox from './DatePickerBox';
 
-export {InputBox, IconBox, ButtonBox, ErrorBox, PickerBox, DatePickerBox};
+export {
+  InputBox,
+  ImageBox,
+  IconBox,
+  ButtonBox,
+  ErrorBox,
+  PickerBox,
+  DatePickerBox,
+};
